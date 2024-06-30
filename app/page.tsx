c2780b5ef5b9184d6fbc1b3187b2fabebc754780
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-06-30 20:05:34 +0200

    addition of the logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import HeroSection from "@/components/HeroSection";
 import React from "react";
+import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 const App = () => {
   return (
-    <div className="w-full p-8 md:p-28">
+    <div className="w-full p-8 md:px-24 md:py-8 ">
+      <div className="w-44 mx-auto">
+        <Image src="/weavimovielogo.png" width={125} height={125} alt="logo" />
+        <span>Powered by Weaviate</span>
+      </div>
       <HeroSection />
     </div>
   );
