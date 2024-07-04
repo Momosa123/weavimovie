1fe02e21661e1c5f00d2b8bc3f196a435b283509
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-07-04 13:42:09 +0200

    editing metadata

--- components/BackgroundCarrousel.tsx
+++ components/BackgroundCarrousel.tsx
@@ -25,7 +25,7 @@ const BackgroundCarousel = () => {
         {displayedMovies.map((movie, index) => (
           <div key={index}>
             <div
-              className="carousel-slide carrousel-zoom"
+              className="carousel-slide"
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
               }}
