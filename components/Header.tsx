05b9b51b562ac975df3889a2c473ddd2e71f1187
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-07-04 22:55:01 +0200

    sentence powerd by weaviate

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import FormComponent from "./FormComponent";
 import Link from "next/link";
 const Header = () => {
   return (
-    <div className="pt-8 md:pt-18 px-7 relative  max-w-4xl mx-auto mb-10 text-white">
-      <h1 className="mb-10">
+    <div className="pt-8 md:pt-4 px-7 mb-8 relative  max-w-4xl mx-auto text-white">
+      <h1 className="mb-5">
         <Link href="/" className="w-fit">
           {" "}
           <span className="text-4xl	mb-4 font-extrabold block bg-green-radial text-transparent bg-clip-text ">
@@ -17,6 +17,22 @@ const Header = () => {
             AI semantic search.{" "}
           </span>
         </span>
+        <div className="flex flex-row justify-center sm:justify-start   items-center gap-3">
+          <div className=" text-lg sm:block hidden">powered by</div>
+          <a
+            className="hover:scale-105 transition-all duration-250"
+            href="https://weaviate.io"
+            target="_blank"
+          >
+            <Image
+              src="/weaviatelogo.png"
+              alt="Weaviate logo"
+              width={125}
+              height={125}
+            />
+          </a>
+          <div className="text-lg sm:block hidden">Weaviate</div>
+        </div>
       </h1>
       <FormComponent />
     </div>
